b2a925ef92b840ba5bbefee1c2a2ba44a2d895f2
DableUTeeF <palm22180@gmail.com>
2024-10-10 15:41:06 +0700

    Changed to table

--- web_ui/views.py
+++ web_ui/views.py
@@ -8,8 +8,44 @@ from django.http import StreamingHttpResponse
 import mimetypes
 from wsgiref.util import FileWrapper
 import pathlib
+from io import BytesIO
+import tarfile
+
 
 srcs = '/nas' if os.path.exists('/nas') else '/home/palm/PycharmProjects/django-file-server/demofolder'
+
+
+class FileStream:
+    def __init__(self):
+        self.buffer = BytesIO()
+        self.offset = 0
+
+    def write(self, s):
+        self.buffer.write(s)
+        self.offset += len(s)
+
+    def tell(self):
+        return self.offset
+
+    def close(self):
+        self.buffer.close()
+
+    def pop(self):
+        s = self.buffer.getvalue()
+        self.buffer.close()
+        self.buffer = BytesIO()
+        return s
+
+    @classmethod
+    def yield_tar(cls, file_data_iterable):
+        stream = FileStream()
+        tar = tarfile.TarFile.open(mode='w|', fileobj=stream, bufsize=tarfile.BLOCKSIZE)
+        for file_name, file_size, file_date, file_data in file_data_iterable:
+            tar_info = tarfile.TarInfo(file_name)
+            tar_info.size = int(file_size)
+            tar_info.mtime = file_date
+            tar.addfile(tar_info)
+            yield stream.pop()
 
 
 def get_context(request):
@@ -24,12 +60,12 @@ def get_context(request):
 def sizeof_fmt(num, suffix="B"):
     for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
         if abs(num) < 1024.0:
-            return f"{int(num)}{unit}{suffix}"
+            return f"{int(num)} {unit}{suffix}"
         num /= 1024.0
-    return f"{int(num)}Yi{suffix}"
+    return f"{int(num)} Yi{suffix}"
 
 
-def download(path):
+def download_single_file(path):
     filename = os.path.basename(path)
     chunk_size = 8192
     response = StreamingHttpResponse(
@@ -41,6 +77,21 @@ def download(path):
     )
     response["Content-Length"] = os.path.getsize(path)
     response["Content-Disposition"] = f"attachment; filename={filename}"
+    return response
+
+def download_directory(files):
+    file_data_iterable = [(
+        file.name,
+        file.size,
+        file.date.timestamp(),
+        file.data
+    ) for file in files]
+
+    response = StreamingHttpResponse(
+        FileStream.yield_tar(file_data_iterable),
+        content_type="application/x-tar"
+    )
+    response["Content-Disposition"] = 'attachment; filename="streamed.tar"'
     return response
 
 def get_navigator(path):
@@ -60,7 +111,7 @@ def files(request, path=''):
     if not os.path.isdir(os.path.join(srcs, path)):
         if path.endswith('/'):
             path = path[:-1]
-        return download(os.path.join(srcs, path))
+        return download_single_file(os.path.join(srcs, path))
     
     context = {
         # 'current_path': '/' + path
@@ -69,6 +120,9 @@ def files(request, path=''):
     files = []
     dirs = []
     for file in sorted(os.listdir(os.path.join(srcs, path))):
+        if len(dirs) + len(files) > 100:
+            context['redacted'] = True
+            break
         if os.path.isdir(os.path.join(srcs, path, file)):
             # dirs.append(os.path.join(path, file) + '/')
             try:
